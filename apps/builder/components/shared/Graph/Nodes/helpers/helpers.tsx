398e45d1ed852a0482cae3bdc157a23ef008a676
Daniel <daniel.ferreira@octadesk.com>
2025-11-14 15:11:59 -0300

    fix: merge commit

--- apps/builder/components/shared/Graph/Nodes/helpers/helpers.tsx
+++ apps/builder/components/shared/Graph/Nodes/helpers/helpers.tsx
@@ -72,20 +72,19 @@ export const getValidationMessages = (step: Step): Array<ValidationMessage> => {
       }
     }
 
-<<<<<<< HEAD
     if (OctaStepType.CONVERSATION_TAG === step.type) {
-      if (!step?.options?.tags || step?.options?.tags.length === 0 || step?.options?.tags[0]?._id === '') {
-        data.push(
-          {
-            message: undefined,
-            min: { value: 1 },
-          }
-        )
+      if (
+        !step?.options?.tags ||
+        step?.options?.tags.length === 0 ||
+        step?.options?.tags[0]?._id === ''
+      ) {
+        data.push({
+          message: undefined,
+          min: { value: 1 },
+        })
       }
     }
 
-=======
->>>>>>> feat/KSC-1034-interpret-data-with-ai
     if (OctaStepType.OFFICE_HOURS === step.type) {
       data.push({
         message: step?.options?.id,
