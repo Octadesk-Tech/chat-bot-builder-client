6e1d24fa9f0957b1c5d59c186caf63f40f0893c6
Caroline Clara <carolineclaradsl@gmail.com>
2025-11-27 16:46:55 -0300

    fix: remove incorrect usage

--- apps/builder/components/shared/Graph/Nodes/StepNode/StepNode/StepNode.tsx
+++ apps/builder/components/shared/Graph/Nodes/StepNode/StepNode/StepNode.tsx
@@ -156,7 +156,6 @@ export const StepNode = ({
     )
   }, [connectingIds, step.blockId, step.id])
 
-  useIframeOverlayEvent(isPopoverOpened, 'popover', step.id)
   useIframeOverlayEvent(isModalOpen, 'modal', `step-settings-${step.id}`)
 
   const handleModalClose = () => {
